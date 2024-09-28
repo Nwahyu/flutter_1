0de421f096272b3d0981273e3daea6c5a3b1d41d
affandynaufal6 <affandynaufal6@gmail.com>
2024-09-28 14:25:59 +0000

    add new

---
+++ lib/main_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/detail_screen.dart';
+import 'package:myapp/rincian.dart';
+
+class Mainscreen extends StatelessWidget {
+  // final Produk produk;
+  const Mainscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ini title'),
+        ),
+        body: ListView.builder(itemBuilder: (context, index) {
+          final Produk produk = produkList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen(
+                  produk: produk,
+                );
+              }));
+            },
+            child: Card(
+                child: Row(children: [
+              Expanded(flex: 1, child: Image.network(produk.ikon)),
+              Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [Text(produk.nama)],
+                  ))
+            ])),
+          );
+        },
+        itemCount: produkList.length,
+        )
+        );
+  }
+}
